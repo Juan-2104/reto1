7d249a0b7ada78809467f9d90609639304be5686
Juan-2104 <juangonzaleznegocios@gmail.com>
2021-02-27 01:36:37 -0500

    Mejorada la página

--- artista.js
+++ artista.js
@@ -1,1 +1,12 @@
-alert("si funciona martino")
+function descripcion(){
+    alert("Logo Martin Garrix");
+}
+function info1(){
+    alert("Martin Garrix");
+}
+function info2(){
+    alert("Logo Martin Garrix");
+}
+function info3(){
+    alert("Martin Garrix en un madness");
+}
